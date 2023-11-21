19323b893df52045e34fa4cc0344be62abeac840
sylee6529 <sylee6529@gmail.com>
2023-11-21 01:39:57 +0000

    chore: 주석 추가

--- hostinfo/hostinfo.c
+++ hostinfo/hostinfo.c
@@ -11,8 +11,7 @@ void Getnameinfo(const struct sockaddr *sa, socklen_t salen, char *host,
                  size_t hostlen, char *serv, size_t servlen, int flags) {
     int rc;
 
-    if ((rc = getnameinfo(sa, salen, host, hostlen, serv,
-                          servlen, flags)) != 0)
+    if ((rc = getnameinfo(sa, salen, host, hostlen, serv, servlen, flags)) != 0)
         gai_error(rc, "Getnameinfo error");
 }
 
@@ -29,7 +28,7 @@ void gai_error(int code, char *msg) {
 
 /* NSLOOKUP과 유사한 동작을 하는 코드 - 도메인 이름을 입력하면 IP 주소를 출력 */
 int main(int argc, char **argv) {
-    struct addrinfo *p, *listp, hints;
+    struct addrinfo *p, *listp, hints;  // hints는 선택적으로 사용 가능한 인자, getaddrinfo가 리턴하는 소켓 주소 리스트의 상세한 제어를 위한 구조체
     char buf[MAXLINE];
     int rc, flags;
 
@@ -40,14 +39,14 @@ int main(int argc, char **argv) {
 
     memset(&hints, 0, sizeof(struct addrinfo));  // hints 구조체를 초기화
     hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_socktype = SOCK_STREAM;  // SOCK_STREAM 으로 설정하면 리스트가 각 고유의 주소에 대해 자신의 소켓 주소가
+                                      // 연결의 끝점으로 사용될 수 있는 addrinfo 구조체는 최대 한 개로 제한
     if ((rc = getaddrinfo(argv[1], NULL, &hints, &listp)) != 0) {
         fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(rc));
         exit(1);
     }
 
-    flags = NI_NUMERICHOST;  // getnameinfo()는 기본적으로 /etc/services에 찾아가서 포트번호 대신 서비스 이름을 탐색하여 리턴하는데,
-                             // 이 플래그를 설정하면 탐색과정 생략, 포트번호만 간단히 리턴
+    flags = NI_NUMERICHOST;  // 플래그를 NI_NEMERICHOST로 설정하면, addrinfo의 service 인자는 포트번호여야 한다
 
     // addrinfo 구조체 리스트를 하나씩 방문하며 찾는다
     for (p = listp; p; p = p->ai_next) {
