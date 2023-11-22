8d6abf9e629d4db879a2a092f1d9400512447daa
sylee6529 <sylee6529@gmail.com>
2023-11-22 01:09:21 +0000

    feat: tiny server 1차(과제 전)

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -4,7 +4,33 @@
 /* $begin adder */
 #include "csapp.h"
 
+/* 2개의 정수를 더하는 프로그램*/
 int main(void) {
-  exit(0);
+    char *buf, *p;
+    char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
+    int n1 = 0, n2 = 0;
+
+    if ((buf = getenv("QUERY_STRING")) != NULL) {
+        p = strchr(buf, '&');
+        *p = '\0';
+        strcpy(arg1, buf);
+        strcpy(arg2, p + 1);
+        n1 = atoi(arg1);
+        n2 = atoi(arg2);
+    }
+
+    sprintf(content, "QUERY_STIRNG=%s", buf);
+    sprintf(content, "Welcome to add.com: ");
+    sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
+    sprintf(content, "%sThe Answer is: %d + %d = %d\r\n<p>", content, n1, n2, n1 + n2);
+    sprintf(content, "%sThanks for visiting! \r\n", content);
+
+    printf("Connection: close\r\n");
+    printf("Content-length: %d\r\n", (int)strlen(content));
+    printf("Content-type: text/html\r\n\r\n");
+    printf("%s", content);
+    fflush(stdout);
+
+    exit(0);
 }
 /* $end adder */
